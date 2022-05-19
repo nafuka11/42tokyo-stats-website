83fd4572f5b94cace60034346525052560b60c88
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-05-19 18:36:42 +0900

    feat(components): add FutureStudentCount

---
+++ components/FutureStudentCount.tsx
@@ -0,0 +1,44 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { StudentStatusData } from "../types/StudentStatusData";
+
+type Props = {
+  studentStatus: StudentStatusData;
+};
+
+const FutureStudentCount = (props: Props) => {
+  const { studentStatus } = props;
+  const futureBeginAtList = Object.keys(studentStatus).filter(
+    (key) => studentStatus[key].future
+  );
+
+  return (
+    <>
+      <Typography>今後入学する学生数</Typography>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>入学日</TableCell>
+            <TableCell>学生数</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {futureBeginAtList.map((beginAt) => (
+            <TableRow key={beginAt}>
+              <TableCell>{beginAt}</TableCell>
+              <TableCell>{studentStatus[beginAt].future}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
+
+export default FutureStudentCount;
