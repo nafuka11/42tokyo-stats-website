1cb610165b620a4195491a6c9dd42bdb7a3c7039
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-05-19 18:23:32 +0900

    feat(components): add LevelPieChart

---
+++ components/LevelPieChart.tsx
@@ -0,0 +1,52 @@
+import HighchartsReact from "highcharts-react-official";
+import Highcharts from "highcharts";
+import { LevelUserData } from "../types/LevelBeginAtData";
+import { LEVEL_COLORS } from "../constants/colors";
+
+type Props = {
+  levels: LevelUserData[];
+  beginAt: string;
+};
+
+const LevelPieChart = (props: Props) => {
+  const { levels, beginAt } = props;
+  const options: Highcharts.Options = {
+    title: {
+      text: beginAt,
+    },
+    tooltip: {
+      headerFormat: "Lv. {point.point.x}<br/>",
+      pointFormat: "{point.y}人 (<b>{point.percentage:.1f}%</b>)",
+    },
+    chart: {
+      height: "100%",
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          enabled: true,
+          alignTo: "connectors",
+          format: "Lv. {point.x}<br>({point.y}人)",
+        },
+        size: "75%",
+        borderWidth: 0.2,
+        borderColor: "#000000",
+      },
+    },
+    series: [
+      {
+        type: "pie",
+        name: "Students",
+        data: levels.map((levelUser) => ({
+          x: levelUser.level,
+          y: levelUser.count,
+          color: LEVEL_COLORS[levelUser.level],
+        })),
+      },
+    ],
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
+};
+
+export default LevelPieChart;
