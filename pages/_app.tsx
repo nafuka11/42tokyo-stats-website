a7e80c6e9f6d3a1a69cdb8b4d4d4e5ba02614267
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-06-01 00:32:57 +0900

    feat: log out from appbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import MenuBar from "../components/MenuBar";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <MenuBar />
       <Component {...pageProps} />
     </SessionProvider>
   );
