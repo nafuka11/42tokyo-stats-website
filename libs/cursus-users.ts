39ec90f95413eacb5178b8afc8f320453c276940
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-06-01 17:26:35 +0900

    fix(cursus-users): remove unneccesary resolve

--- libs/cursus-users.ts
+++ libs/cursus-users.ts
@@ -5,7 +5,7 @@ import { CursusUser } from "../types/CursusUser";
 const cursusUsersPath = resolve(process.cwd(), "contents", "cursus_users.json");
 
 export const fetchCursusUsers = (): CursusUser[] => {
-  const fileContents = fs.readFileSync(resolve(cursusUsersPath), "utf8");
+  const fileContents = fs.readFileSync(cursusUsersPath, "utf8");
   const cursusUsers: CursusUser[] = JSON.parse(fileContents);
   const filteredCursusUsers = cursusUsers.filter((cursusUser) =>
     isStudent(cursusUser)
