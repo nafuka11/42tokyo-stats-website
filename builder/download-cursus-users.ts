cca5ca4eb8de59e594c06bc58b2963634f48d94f
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-06-01 21:52:18 +0900

    feat(builder): save json with object timeStamp as name

--- builder/download-cursus-users.ts
+++ builder/download-cursus-users.ts
@@ -1,11 +1,14 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
-import { format } from "date-fns";
 import sub from "date-fns/sub";
 import { resolve } from "path";
 import dotenv from "dotenv";
+import { format, utcToZonedTime } from "date-fns-tz";
 
+const TIMEZONE = "Asia/Tokyo";
+const TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 const MAX_DOWNLOAD_RETRY_COUNT = 10;
+const CURSUS_USERS_DIR = resolve(process.cwd(), "contents");
 
 dotenv.config({ path: resolve(process.cwd(), ".env.local") });
 
@@ -21,16 +24,22 @@ initializeApp({
   storageBucket: process.env.BUCKET_NAME,
 });
 
-const cursusUsersPath = resolve(process.cwd(), "contents", "cursus_users.json");
-
 const downloadCursusUsers = async () => {
   const bucket = getStorage().bucket();
   let now = new Date();
   for (let i = 0; i < MAX_DOWNLOAD_RETRY_COUNT; i++) {
-    const filename = `v1/cursus_users/${format(now, "yyyy-MM-dd")}.json`;
+    const filename = `v1/cursus_users/${format(now, "yyyy-MM-dd", {
+      timeZone: TIMEZONE,
+    })}.json`;
     console.log(`try to download: ${filename}`);
     try {
       const file = bucket.file(filename);
+
+      const [metadata] = await file.getMetadata();
+      const timeCreated = new Date(metadata.timeCreated);
+      const timeStamp = getTimeStamp(timeCreated);
+      const cursusUsersPath = resolve(CURSUS_USERS_DIR, `${timeStamp}.json`);
+
       await file.download({ destination: cursusUsersPath });
       break;
     } catch (e: any) {
@@ -38,6 +47,14 @@ const downloadCursusUsers = async () => {
     }
     now = sub(now, { days: 1 });
   }
+};
+
+const getTimeStamp = (date: Date) => {
+  const timeCreatedLocal = utcToZonedTime(date, TIMEZONE);
+  const timeStr = format(timeCreatedLocal, TIME_FORMAT, {
+    timeZone: TIMEZONE,
+  });
+  return timeStr;
 };
 
 (async () => await downloadCursusUsers())();
