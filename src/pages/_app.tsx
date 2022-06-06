ce3cec9177f873826c5dd1a54285fb58be53ccb2
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-06-06 16:57:52 +0900

    feat: add NextHeadSeo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import NextHeadSeo from "next-head-seo";
 import MenuBar from "../components/common/MenuBar";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <NextHeadSeo
+        title="42Tokyo Stats"
+        description="42Tokyoの統計情報を表示するWebサイト"
+        canonical={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}`}
+        og={{ type: "website" }}
+      />
       <MenuBar />
       <Component {...pageProps} />
     </SessionProvider>
