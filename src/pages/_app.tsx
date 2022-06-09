f4a637cb7d47a904cdef35d75e50ae0e2ba54eba
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-06-09 22:49:33 +0900

    feat: add theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,23 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import NextHeadSeo from "next-head-seo";
+import { ThemeProvider } from "@mui/material";
 import MenuBar from "../components/common/MenuBar";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <NextHeadSeo
-        title="42Tokyo Stats"
-        description="42Tokyoの統計情報を表示するWebサイト"
-        og={{ type: "website" }}
-      />
-      <MenuBar />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ThemeProvider theme={theme}>
+      <SessionProvider session={session}>
+        <NextHeadSeo
+          title="42Tokyo Stats"
+          description="42Tokyoの統計情報を表示するWebサイト"
+          og={{ type: "website" }}
+        />
+        <MenuBar />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ThemeProvider>
   );
 }
 
