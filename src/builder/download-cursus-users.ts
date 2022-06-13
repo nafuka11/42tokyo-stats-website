74c41ba5b893e468e218ef2f0f27d8f6ac891f2f
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-06-13 17:03:34 +0900

    refactor: pass date as argument in downloadCursusUsersJson()

--- src/builder/download-cursus-users.ts
+++ src/builder/download-cursus-users.ts
@@ -1,33 +1,28 @@
 import sub from "date-fns/sub";
-import { format } from "date-fns-tz";
 import { downloadCursusUsersJson } from "../repositories/cloud-storage";
 import { writeContents } from "../repositories/local-file";
 import { aggregateContents } from "../services/cursus-users";
-import { TIMEZONE } from "../constants/time";
 
 const MAX_DOWNLOAD_RETRY_COUNT = 10;
 
 const downloadCursusUsers = async () => {
+  const { cursusUsers, timeCreated } = await downloadLatestData();
+  const contents = aggregateContents(cursusUsers, timeCreated);
+  await writeContents(contents);
+};
+
+const downloadLatestData = async () => {
   let now = new Date();
   for (let i = 0; i < MAX_DOWNLOAD_RETRY_COUNT; i++) {
-    const filename = `v1/cursus_users/${format(now, "yyyy-MM-dd", {
-      timeZone: TIMEZONE,
-    })}.json`;
     try {
-      console.log(`try to download: ${filename}`);
-      const { cursusUsers, timeCreated } = await downloadCursusUsersJson(
-        filename
-      );
-
-      const contents = aggregateContents(cursusUsers, timeCreated);
-      await writeContents(contents);
-
-      break;
+      const { cursusUsers, timeCreated } = await downloadCursusUsersJson(now);
+      return { cursusUsers, timeCreated };
     } catch (e: any) {
       // do nothing
     }
     now = sub(now, { days: 1 });
   }
+  throw new Error("Failed to download cursusUsers from Google Cloud Storage");
 };
 
 (async () => await downloadCursusUsers())();
