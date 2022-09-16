962392c8a5c5907c1d9647b2be5f712e93b8f823
nafuka <42476527+nafuka11@users.noreply.github.com>
2022-09-16 15:51:05 +0900

    fix(pages): fix AppProps type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import NextHeadSeo from "next-head-seo";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import MenuBar from "../components/common/MenuBar";
 import { theme } from "../styles/theme";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   return (
     <ThemeProvider theme={theme}>
       <SessionProvider session={session}>
