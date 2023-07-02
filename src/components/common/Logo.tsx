a9de4f6e1fcc10897b8f38cf34c7dfbc763ebf1b
nafuka <42476527+nafuka11@users.noreply.github.com>
2023-07-02 17:35:20 +0900

    fix: remove 'a' tags inside Link components for Next.js 13

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -4,11 +4,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/" passHref>
-      <Typography
-        sx={{ fontWeight: 500, color: "#444" }}
-        variant="h6"
-        component="a"
-      >
+      <Typography sx={{ fontWeight: 500, color: "#444" }} variant="h6">
         42Tokyo Stats
       </Typography>
     </Link>
