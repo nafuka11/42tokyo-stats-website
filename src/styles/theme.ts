bd0629c7a28a393dc13468b30c93d4d063df4fab
nafuka <42476527+nafuka11@users.noreply.github.com>
2025-12-07 10:55:32 +0900

    chore: update mui (v5 -> v6)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme, responsiveFontSizes } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 const userTheme = createTheme({
   palette: {
